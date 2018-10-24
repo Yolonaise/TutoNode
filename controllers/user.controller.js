51d2b78df4ad88da979274bdf85a26e41aa71695
Yolonaise <arnaud.schaal9@gmail.com>
2018-10-24 23:10:26 +0200

    Update controllers/user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,51 +1,51 @@
 const User = require('../models/user.model');
 const Jwt = require('jsonwebtoken');
 
-exports.login = function(req, res){
-    User.findOne({ pseudo : req.body.pseudo, password : req.body.password }, function(err, user){
-        if(err){
+exports.login = function (req, res) {
+    User.findOne({ pseudo: req.body.pseudo, password: req.body.password }, function (err, user) {
+        if (err) {
             res.send(err);
         }
         else {
-            var token = Jwt.sign({username: user.pseudo, password: user.password }, req.headers.apikey,{expiresIn: 120});
-            res.send({token : token});
+            var token = Jwt.sign({ username: user.pseudo, password: user.password }, req.headers.apikey, { expiresIn: "2 days" });
+            res.send({ token: token });
         }
     });
 }
 
-exports.signin = function(req, res){
-    User.findOne({ email : req.body.email }, function(err, user){
-        if(err){
+exports.signin = function (req, res) {
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) {
             res.send(err);
         }
         else {
-            if(user != null)
+            if (user != null)
                 res.send('Email is already taken');
-                return;
+            return;
         }
     });
 
-    User.findOne({ pseudo : req.body.pseudo }, function(err, user){
-        if(err){
+    User.findOne({ pseudo: req.body.pseudo }, function (err, user) {
+        if (err) {
             return res.send(err);
         }
         else {
-            if(user != null)
+            if (user != null)
                 return res.send('pseudo is already taken');
 
             let user = new User({
-                pseudo : req.body.pseudo, 
-                nom : req.body.nom,
-                prenom : req.body.prenom,
-                email : req.body.email,
-                password : req.body.password
+                pseudo: req.body.pseudo,
+                nom: req.body.nom,
+                prenom: req.body.prenom,
+                email: req.body.email,
+                password: req.body.password
             });
-        
-            user.save(function(err){
-                if(err){
+
+            user.save(function (err) {
+                if (err) {
                     res.send(err);
                 }
-                else{
+                else {
                     res.send('user successfully created');
                 }
             });
